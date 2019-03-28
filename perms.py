89e303a5105a8196bf7cc8e2b79439ce364bcf40
Jonathan Lam <jonathanlam@users.noreply.github.com>
2019-03-28 19:26:16 +1100

    necklace functions

--- perms.py
+++ perms.py
@@ -7,6 +7,28 @@ def subword_from_word(word, length=None):
     The function returns a list of these words
     """
     length = len(word) if length is None else length
-    a = [''.join(p) for p in permutations(word, length)]
-    a = list(set(a))
+    a = {''.join(p) for p in permutations(word, length)}
+    a = list(a)
     return a
+
+# Returns a unique representation of all the rotations and reflections of a particular word
+def necklace_lexo(word):
+    temp = word + word
+    reverse = word[::-1]+word[::-1]
+    list1 = [temp[i:i+len(word)] for i in range(len(word))]
+    list1.extend([reverse[i:i+len(word)] for i in range(len(word))])
+    return min(list1)
+
+def test_necklace_lexo():
+    equiv = ["ABC", "BCA", "CAB", "CBA", "BAC", "ACB"]
+    for item in equiv:
+        assert(lexo(item) == "ABC")
+    equiv = ["ABCC", "BCCA", "CCAB", "CABC", "CCAB", "ACCB", "BACC", "CBAC"]
+    for item in equiv:
+        assert(lexo(item) == "ABCC")
+    print("test complete")
+    
+def necklace_count(word):
+    total = {''.join(lexo(p)) for p in permutations(word)}
+    total = list(total)
+    return total
